a3fcd1abde8d8fcc8e620206ada10b163b6c8523
Zane <zanesuhi@gmail.com>
2017-08-05 10:34:39 +0200

    Updated

--- jQuery.js
+++ jQuery.js
@@ -10,3 +10,25 @@ $('li strong');          // Descendant selector
 $('em, i');              // Multiple selector
 $('a[target="_blank"]'); // Attribute selector
 $('p:nth-child(2)');     // Pseudo-class selector
+
+//Selecting an element which was referred inside of the original selector
+$('div').click(function(event){ 
+  $(this);
+});
+
+//Traversing
+//First we need to make a selection where we will traverse relatively
+$('div').not('.type, .collection');
+//Selecting all 'div' elements that DON'T have a class of .type and .collection
+
+//We can also chain together traversing
+$('div').not('.type, .collection').parent();
+//Here we select only the parent element of the elements that DON'T have 
+//.type and .collection
+
+//Getting and setting information
+// Gets the value of the alt attribute
+$('img').attr('alt');
+
+// Sets the value of the alt attribute
+$('img').attr('alt', 'Wild kangaroo');
