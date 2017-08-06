6dc72af55b8e5217466d118d4dc6cbac45ea7513
Zane <zanesuhi@gmail.com>
2017-08-06 09:01:58 +0200

    Updated

--- jQuery.js
+++ jQuery.js
@@ -37,3 +37,31 @@ $('img').attr('alt', 'Wild kangaroo');
 $('li:even').addClass('even-item');//Used to add a class to all even items in a list
 $('p').removeClass(); //Used to remove all classes from any paragraphs
 $('abbr').attr('title', 'Hello World'); //Used to set all abbr attributes to Hello World
+
+//Style manipulation syntax
+$('h1 span').css('font-size', 'normal');
+$('div').css({
+  fontSize: '13px', 
+  background: '#f60'
+});
+$('header').height(200);
+$('.extend').height(30 + 'em');
+
+//DOM manipulation syntax
+$('section').prepend('<h3>Featured</h3>'); //Adding text just before the h3
+$('a[target="_blank"]').after('<em>New window.</em>'); //Adding a new em element just after the link
+$('h1').text('Hello World'); //Changing the text of h1 to Hello World
+
+//Events
+$('li').click(function(event){
+  $(this).addClass('saved-item');
+}); //When the list item is clicked it will get the class of saved-item through the this keyword
+
+//Effect callback syntax
+$('.error').show('slow', 'linear', function(event){
+  $('.error .status').text('Continue');
+});
+//The callback function should be placed after the duration or easing, if 
+//either exist. Inside this function 
+//new events or effects may be placed, each following their own required syntax.
+
